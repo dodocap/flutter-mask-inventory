0a0482eba199b2503c3292b6c6f3fec7138dfb83
dodocap <dodocap@naver.com>
2023-12-29 17:10:55 +0900

    feat: Refresh 버튼 및 기능 추가

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_mask_inventory/model/mask.dart';
 import 'package:flutter_mask_inventory/ui/main/main_view_model.dart';
 
 class MainScreen extends StatefulWidget {
-
   const MainScreen({super.key});
 
   @override
@@ -22,33 +21,40 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('마스크 재고 관리')),
-      body: StreamBuilder(
-        initialData: false,
-        stream: _mainViewModel.isLoading,
-        builder: (context, snapshot) {
-          if(snapshot.data == true) {
-            return const Center(child: CircularProgressIndicator());
-          }
+        appBar: AppBar(
+          title: const Text('마스크 재고 관리'),
+          actions: [
+            IconButton(
+              onPressed: () => _mainViewModel.getMaskInventory(),
+              icon: const Icon(Icons.refresh),
+            )
+          ],
+        ),
+        body: StreamBuilder(
+          initialData: false,
+          stream: _mainViewModel.isLoading,
+          builder: (context, snapshot) {
+            if (snapshot.data == true) {
+              return const Center(child: CircularProgressIndicator());
+            }
 
-          final List<Mask> list = _mainViewModel.maskList;
-          return ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (context, index) {
-              final Mask mask = list[index];
-              return ListTile(
-                title: Text(mask.storeName),
-                subtitle: Text(mask.address),
-                trailing: Text(
-                  mask.remainStatus.statusText,
-                  style: TextStyle(color: mask.remainStatus.color),
-                ),
-                onTap: () => print(mask),
-              );
-            },
-          );
-        },
-      )
-    );
+            final List<Mask> list = _mainViewModel.maskList;
+            return ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                final Mask mask = list[index];
+                return ListTile(
+                  title: Text(mask.storeName),
+                  subtitle: Text(mask.address),
+                  trailing: Text(
+                    mask.remainStatus.statusText,
+                    style: TextStyle(color: mask.remainStatus.color),
+                  ),
+                  onTap: () => print(mask),
+                );
+              },
+            );
+          },
+        ));
   }
 }
