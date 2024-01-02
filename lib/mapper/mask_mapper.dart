5eab8bbf292b75c6e29085d908d0818f51f1e4f9
dodocap <dodocap@naver.com>
2024-01-02 10:20:27 +0900

    feat: 여분 색상 표현 변경

--- lib/mapper/mask_mapper.dart
+++ lib/mapper/mask_mapper.dart
@@ -3,11 +3,11 @@ import 'package:flutter_mask_inventory/dto/mask_dto.dart';
 import 'package:flutter_mask_inventory/model/mask.dart';
 
 enum MaskRemainStatus {
-  plenty('plenty', '100개 이상', Colors.green),
-  some('some', '30~100개', Colors.yellow),
-  few('few', '2~30개', Colors.red),
-  empty('empty', '1개 이하', Colors.grey),
-  breaks('break', '매진', Colors.black),
+  plenty('plenty', '100개 이상', Colors.blue),
+  some('some', '30~100개', Colors.green),
+  few('few', '2~30개', Colors.amber),
+  empty('empty', '1개 이하', Colors.red),
+  breaks('break', '매진', Colors.grey),
   other('other', '재고파악불가', Colors.black);
 
   const MaskRemainStatus(this.name, this.statusText, this.color);
